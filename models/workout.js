d7df2db6509201353a0fc05b204f9e4d6663e504
Pradeep Raghuvanshi <149401110+ipradeepraghuvansh@users.noreply.github.com>
2026-05-04 09:40:11 +0530

    Update workout.js

---
+++ models/workout.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>workout interface</p>
+</body>
+</html>
