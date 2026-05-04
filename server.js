eabb5d4711a5d28d11b06a88f57617fcefabef1c
Pradeep Raghuvanshi <149401110+ipradeepraghuvansh@users.noreply.github.com>
2026-05-04 09:34:07 +0530

    Initialize server with Express and required modules

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+
+
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
